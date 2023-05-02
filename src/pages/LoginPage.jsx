f2f7151c303da692dc37161f43f9bfe92d9e2283
Andrey Shaposhnikov <droniiiandruxa@gmail.com>
2023-05-03 01:26:00 +0300

    HW8 styles

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -26,26 +26,71 @@ const LoginPage = () => {
       </div>
     );
   }
+
+  const styles = {
+    form: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      width: 320,
+      margin: '0 auto',
+      marginBottom: 20,
+      padding: 20,
+      border: '1px solid #bdbdbd',
+      borderRadius: 5,
+    },
+    label: {
+      display: 'flex',
+      flexDirection: 'column',
+      marginBottom: 15,
+      width: '100%',
+    },
+    input: {
+      display: 'inline-block',
+      padding: '8px 12px',
+      marginTop: '10px',
+      fontSize: 16,
+      borderRadius: 5,
+      border: '1px solid #bdbdbd',
+      outline: 'none',
+      width: '100%',
+    },
+    button: {
+      display: 'inline-block',
+      padding: '8px 16px',
+      fontSize: 18,
+      fontWeight: 500,
+      color: '#fff',
+      backgroundColor: '#3f51b5',
+      borderRadius: 5,
+      border: 'none',
+      outline: 'none',
+      cursor: 'pointer',
+      transition: 'background-color 250ms cubic-bezier(0.4, 0, 0.2, 1)',
+      marginTop: 10,
+    },
+    buttonHover: {
+      backgroundColor: '#303f9f',
+    },
+  };
+
   return (
     <div className="flex justify-center items-center h-screen bg-darkMain">
-      <form
-        className="flex gap-6 text-white flex-col w-1/3 p-10 border-blue-600 border-2 rounded-lg"
-        onSubmit={handleSubmit}
-      >
+      <form style={styles.form} onSubmit={handleSubmit}>
         <h1 className="text-center text-2xl">Login Form</h1>
         <input
-          className="border px-2 text-black"
+          style={styles.input}
           name="email"
           placeholder="Email..."
           type="text"
         />
         <input
-          className="border px-2 text-black"
+          style={styles.input}
           name="password"
           placeholder="Password..."
           type="password"
         />
-        <button className="border bg-blue-600 rounded-md">Login</button>
+        <button style={styles.button}>Login</button>
       </form>
     </div>
   );
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -13,33 +13,78 @@ const RegisterPage = () => {
     const password = form.password.value;
     dispatch(registrationThunk({ name, email, password }));
   };
+
+  const styles = {
+    form: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      width: 320,
+      margin: '0 auto',
+      marginBottom: 20,
+      padding: 20,
+      border: '1px solid #bdbdbd',
+      borderRadius: 5,
+    },
+    label: {
+      display: 'flex',
+      flexDirection: 'column',
+      marginBottom: 15,
+      width: '100%',
+    },
+    input: {
+      display: 'inline-block',
+      padding: '8px 12px',
+      marginTop: '10px',
+      fontSize: 16,
+      borderRadius: 5,
+      border: '1px solid #bdbdbd',
+      outline: 'none',
+      width: '100%',
+    },
+    button: {
+      display: 'inline-block',
+      padding: '8px 16px',
+      fontSize: 18,
+      fontWeight: 500,
+      color: '#fff',
+      backgroundColor: '#3f51b5',
+      borderRadius: 5,
+      border: 'none',
+      outline: 'none',
+      cursor: 'pointer',
+      transition: 'background-color 250ms cubic-bezier(0.4, 0, 0.2, 1)',
+      marginTop: 10,
+    },
+    buttonHover: {
+      backgroundColor: '#303f9f',
+    },
+  };
+
   return (
     <div className="flex justify-center items-center h-screen bg-darkMain">
-      <form
-        className="flex gap-6 text-white flex-col w-1/3 p-10 border-blue-600 border-2 rounded-lg"
-        onSubmit={handleSubmit}
-      >
+      <form style={styles.form} onSubmit={handleSubmit}>
         <h1 className="text-center text-2xl">Registration form </h1>
         <input
-          className="border px-2 text-black"
+          style={styles.input}
           name="name"
           type="text"
           placeholder="Name..."
         />
         <input
-          className="border px-2 text-black"
+          style={styles.input}
           name="email"
           autoComplete="off"
           type="text"
           placeholder="Email..."
         />
         <input
-          className="border px-2 text-black"
+          style={styles.input}
           name="password"
           type="password"
           placeholder="Password..."
         />
-        <button className="border bg-blue-600 rounded-md">SignUP</button>
+        <button style={styles.button}>SignUP</button>
       </form>
     </div>
   );
