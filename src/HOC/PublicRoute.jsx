1123994ed0bcc3bc2ea5ebe0891872833ec4c461
Andrey Shaposhnikov <droniiiandruxa@gmail.com>
2023-05-04 13:01:43 +0300

    Logic

--- src/HOC/PublicRoute.jsx
+++ src/HOC/PublicRoute.jsx
@@ -1,17 +1,17 @@
 import { useSelector } from 'react-redux';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 import { selectIsOnline } from '../redux/selectors';
 
 // const publicRoutes = ['/login', '/'];
 
 export const PublicRoute = ({ children }) => {
-  // const location = useLocation();
-  // const fromPage = location.state?.from.pathname || '/';
+  const location = useLocation();
+  const fromPage = location.state?.from.pathname || '/contacts';
   // const isPublicRoute = publicRoutes.includes(fromPage);
   const isOnline = useSelector(selectIsOnline);
 
   if (isOnline) {
-    return <Navigate to="/contacts" />;
+    return <Navigate to={fromPage} />;
   }
   return children;
 };
